e042a6afeeda393dcc5b1d054acdd8f7d9bafe4f
Jonathan E. Emmett <jon@emmettmedia.com>
2022-09-10 16:33:47 -0500

    initial

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+import PasswordGenerator from "./lib/components/PasswordGenerator.svelte";
+
+let name;
+let email;
+let password;
+let password2;
+let generatedPassword = "";
+
+$: generatedPassword && console.log(generatedPassword);
+
+</script>
+
+<div class="container">
+  <h1>Sign up</h1>
+  <form autocomplete="off">
+    <div class="form-group">
+      <input type="text" id="name" class="form-control" placeholder="Name" bind:value={name} />
+    </div>
+    <div class="form-group">
+      <input type="text" id="email" class="form-control" placeholder="Email" bind:value={email} />
+    </div>
+    <div class="form-group">
+      <input type="password" id="password" class="form-control" placeholder="Password" bind:value={name} />
+    </div>
+    <div class="form-group">
+      <input type="password" id="password2" class="form-control" placeholder="Confirm Password" bind:value={name} />
+    </div>
+    <PasswordGenerator generatedPassword={generatedPassword} />
+    <div class="form-group">
+      <button type="submit" class="btn btn-primary">Register</button>
+    </div>
+  </form>
+</div>
+
+<style>
+  
+</style>
---
+++ src/lib/components/PasswordGenerator.svelte
@@ -0,0 +1,76 @@
+<script>
+    import CopyIcon from "./icons/CopyIcon.svelte";
+    export let generatedPassword = "";
+    export let charlen = 16;
+
+    let suggestedPassword = "";
+    let isCopied = false;
+
+    function copy() {
+        let copiedAlert = document.getElementById("copied-alert");
+        navigator.clipboard.writeText(generatedPassword);
+        copiedAlert.style.display = "block";
+        generatedPassword === suggestedPassword;
+        isCopied = true;
+    }
+
+    function generatePassword() {
+        const length = charlen;
+        const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$';
+        let retVal = "";
+        for (let i = 0, n = charset.length; i < length; ++i) {
+            retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+        suggestedPassword = retVal;
+    }
+
+    generatePassword();
+
+    $: suggestedPassword && console.log(suggestedPassword);
+
+    $: isCopied && setTimeout(() => {
+        let copiedAlert = document.getElementById("copied-alert");
+        copiedAlert.style.display = "none";
+        isCopied = false;
+    }, 5000);
+
+</script>
+
+<div>
+    <div class="gpHeader">
+        <h5>Suggested Password</h5>
+        <span id="copied-alert" class="gpCopiedAlert">Copied</span>
+    </div>
+    <div class="gpContainer">
+        <p>{suggestedPassword}</p>
+        <div on:click|preventDefault={() => copy()}><CopyIcon /></div>
+    </div>
+</div>
+
+<style>
+    .gpHeader {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin: 0 0 5px 0;
+    }
+    .gpContainer {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin: 5px 0 15px 0;
+        border: 1px solid #ccc;
+        padding: 1rem;
+        border-radius: 5px;
+    }
+
+    .gpCopiedAlert {
+        display: none;
+        background: #ccc;
+        margin: 0 0 5px 0; 
+        font-size: 12px; 
+        text-align: center;  
+        padding: 5px; 
+        border-radius: 10px; 
+    }
+</style>
